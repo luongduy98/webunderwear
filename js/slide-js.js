f7214b092c34ac90f88b20690a86bab4655dcc0a
luongduy98 <salarysingle@gmail.com>
2018-05-31 01:54:10 +0700

    no message

--- js/slide-js.js
+++ js/slide-js.js
@@ -1,125 +1,118 @@
+'use strict';
 
+$(document).ready(function () {
 
-$(document).ready(function() {
-  
-    var $slider = $(".slider"),
-        $slideBGs = $(".slide__bg"),
-        diff = 0,
-        curSlide = 0,
-        numOfSlides = $(".slide").length-1,
-        animating = false,
-        animTime = 500,
-        autoSlideTimeout,
-        autoSlideDelay = 6000,
-        $pagination = $(".slider-pagi");
-    
-    function createBullets() {
-      for (var i = 0; i < numOfSlides+1; i++) {
-        var $li = $("<li class='slider-pagi__elem'></li>");
-        $li.addClass("slider-pagi__elem-"+i).data("page", i);
-        if (!i) $li.addClass("active");
-        $pagination.append($li);
-      }
-    };
-    
-    createBullets();
-    
-    function manageControls() {
-      $(".slider-control").removeClass("inactive");
-      if (!curSlide) $(".slider-control.left").addClass("inactive");
-      if (curSlide === numOfSlides) $(".slider-control.right").addClass("inactive");
-    };
-    
-    function autoSlide() {
-      autoSlideTimeout = setTimeout(function() {
-        curSlide++;
-        if (curSlide > numOfSlides) curSlide = 0;
-        changeSlides();
-      }, autoSlideDelay);
-    };
-    
-    autoSlide();
-    
-    function changeSlides(instant) {
-      if (!instant) {
-        animating = true;
-        manageControls();
-        $slider.addClass("animating");
-        $slider.css("top");
-        $(".slide").removeClass("active");
-        $(".slide-"+curSlide).addClass("active");
-        setTimeout(function() {
-          $slider.removeClass("animating");
-          animating = false;
-        }, animTime);
-      }
-      window.clearTimeout(autoSlideTimeout);
-      $(".slider-pagi__elem").removeClass("active");
-      $(".slider-pagi__elem-"+curSlide).addClass("active");
-      $slider.css("transform", "translate3d("+ -curSlide*100 +"%,0,0)");
-      $slideBGs.css("transform", "translate3d("+ curSlide*50 +"%,0,0)");
-      diff = 0;
-      autoSlide();
+  var $slides = $('.con__slide').length,
+      topAnimSpd = 650,
+      textAnimSpd = 1000,
+      nextSlideSpd = topAnimSpd + textAnimSpd,
+      animating = true,
+      animTime = 4000,
+      curSlide = 1,
+      nextSlide,
+      scrolledUp;
+
+  setTimeout(function () {
+    animating = false;
+  }, 2300);
+
+  function navigateUp() {
+    if (curSlide > 1) {
+      scrolledUp = true;
+      pagination(curSlide);
+      curSlide--;
     }
-  
-    function navigateLeft() {
-      if (animating) return;
-      if (curSlide > 0) curSlide--;
-      changeSlides();
+  }
+
+  function navigateDown() {
+    if (curSlide < $slides) {
+      scrolledUp = false;
+      pagination(curSlide);
+      curSlide++;
+      console.log(curSlide);
     }
-  
-    function navigateRight() {
-      if (animating) return;
-      if (curSlide < numOfSlides) curSlide++;
-      changeSlides();
-    }
-  
-    $(document).on("mousedown touchstart", ".slider", function(e) {
-      if (animating) return;
-      window.clearTimeout(autoSlideTimeout);
-      var startX = e.pageX || e.originalEvent.touches[0].pageX,
-          winW = $(window).width();
-      diff = 0;
-      
-      $(document).on("mousemove touchmove", function(e) {
-        var x = e.pageX || e.originalEvent.touches[0].pageX;
-        diff = (startX - x) / winW * 70;
-        if ((!curSlide && diff < 0) || (curSlide === numOfSlides && diff > 0)) diff /= 2;
-        $slider.css("transform", "translate3d("+ (-curSlide*100 - diff) +"%,0,0)");
-        $slideBGs.css("transform", "translate3d("+ (curSlide*50 + diff/2) +"%,0,0)");
-      });
-    });
-    
-    $(document).on("mouseup touchend", function(e) {
-      $(document).off("mousemove touchmove");
-      if (animating) return;
-      if (!diff) {
-        changeSlides(true);
-        return;
-      }
-      if (diff > -8 && diff < 8) {
-        changeSlides();
-        return;
-      }
-      if (diff <= -8) {
-        navigateLeft();
-      }
-      if (diff >= 8) {
-        navigateRight();
-      }
-    });
-    
-    $(document).on("click", ".slider-control", function() {
-      if ($(this).hasClass("left")) {
-        navigateLeft();
-      } else {
-        navigateRight();
-      }
-    });
-    
-    $(document).on("click", ".slider-pagi__elem", function() {
-      curSlide = $(this).data("page");
-      changeSlides();
-    });
-    
+  }
+
+  $(window).on('load', function () {
+    $('.con__slide--1').addClass('active');
   });
+
+  function pagination(slide, target) {
+    animating = true;
+    // Check if pagination was triggered by scroll/keys/arrows or direct click. If scroll/keys/arrows then check if scrolling was up or down.
+    if (target === undefined) {
+      nextSlide = scrolledUp ? slide - 1 : slide + 1;
+    } else {
+      nextSlide = target;
+    }
+    ////////// Slides //////////
+    $('.con__slide--' + slide).removeClass('active');
+
+    setTimeout(function () {
+      $('.con__slide--' + nextSlide).addClass('active');
+    }, nextSlideSpd);
+
+    ////////// Nav //////////
+    $('.con__nav-item--' + slide).removeClass('nav-active');
+    $('.con__nav-item--' + nextSlide).addClass('nav-active');
+
+    setTimeout(function () {
+      animating = false;
+    }, animTime);
+  }
+
+  // Mouse wheel trigger
+  $(document).on('mousewheel DOMMouseScroll', function (e) {
+    var delta = e.originalEvent.wheelDelta;
+    if (animating) return;
+    // Mouse Up
+    if (delta > 0 || e.originalEvent.detail < 0) {
+      navigateUp();
+    } else {
+      navigateDown();
+    }
+  });
+
+  // Direct trigger
+  $(document).on("click", ".con__nav-item:not(.nav-active)", function () {
+    // Essential to convert target to a number with +, so curSlide would be a number
+    var target = +$(this).attr('data-target');
+    if (animating) return;
+    pagination(curSlide, target);
+    curSlide = target;
+  });
+
+  // Arrow trigger
+  $(document).on('click', '.con__nav-scroll', function () {
+    var target = $(this).attr('data-target');
+    if (animating) return;
+    if (target === 'up') {
+      navigateUp();
+    } else {
+      navigateDown();
+    }
+  });
+
+  // Key trigger
+  $(document).on("keydown", function (e) {
+    if (animating) return;
+    if (e.which === 38) {
+      navigateUp();
+    } else if (e.which === 40) {
+      navigateDown();
+    }
+  });
+
+  var topLink = $(".con__slide--4-top-h-link"),
+      botLink = $(".con__slide--4-bot-h-link");
+  $(".con__slide--4-top-h-link, .con__slide--4-bot-h-link").on({
+    mouseenter: function mouseenter() {
+      topLink.css('text-decoration', 'underline');
+      botLink.css('text-decoration', 'underline');
+    },
+    mouseleave: function mouseleave() {
+      topLink.css('text-decoration', 'none');
+      botLink.css('text-decoration', 'none');
+    }
+  });
+});
